858c5dab2469fd83866ea2fd45955733563c9e7e
kukidevs <kukidevs@gmail.com>
2023-08-31 01:45:33 +0400

    Changed search UI

--- app/src/main/java/ru/practicum/android/diploma/features/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/ui/SearchFragment.kt
@@ -9,13 +9,15 @@ import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
-class SearchFragment: Fragment(R.layout.fragment_search) {
-//    private val viewModel by viewModel<SearchViewModel>()
+class SearchFragment : Fragment(R.layout.fragment_search) {
+    //    private val viewModel by viewModel<SearchViewModel>()
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -26,22 +28,36 @@ class SearchFragment: Fragment(R.layout.fragment_search) {
             binding.searchInput.text.clear()
         }
         binding.searchInput.doOnTextChanged { text, _, _, _ ->
-            when{
-                text.isNullOrEmpty()-> {
-                    binding.searchRecycler.visibility = View.GONE
-                    binding.searchPlaceholder.visibility = View.VISIBLE
-                    binding.crossSearchInput.visibility = View.GONE
-                    binding.searchInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.search_icon, 0)
+            when {
+                text.isNullOrEmpty() -> {
+                    changeUISearch(true)
 
                 }
+
                 else -> {
-                    binding.searchRecycler.visibility = View.VISIBLE
-                    binding.searchPlaceholder.visibility = View.GONE
-                    binding.crossSearchInput.visibility = View.VISIBLE
-                    binding.searchInput.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+                    changeUISearch(false)
 
                 }
             }
+        }
+    }
+
+    private fun changeUISearch(textEmpty: Boolean) {
+        if (textEmpty) {
+            binding.searchRecycler.visibility = View.GONE
+            binding.searchPlaceholder.visibility = View.VISIBLE
+            binding.crossSearchInput.visibility = View.GONE
+            binding.searchInput.setCompoundDrawablesWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.search_icon,
+                0
+            )
+        } else {
+            binding.searchRecycler.visibility = View.VISIBLE
+            binding.searchPlaceholder.visibility = View.GONE
+            binding.crossSearchInput.visibility = View.VISIBLE
+            binding.searchInput.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
         }
     }
 }
