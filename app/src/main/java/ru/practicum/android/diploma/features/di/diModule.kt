cf3a572f72fdfe6ea668ec7aa9833cd3226f4688
kukidevs <kukidevs@gmail.com>
2023-08-30 15:08:35 +0400

    Created DI file

---
+++ app/src/main/java/ru/practicum/android/diploma/features/di/diModule.kt
@@ -0,0 +1,13 @@
+package ru.practicum.android.diploma.features.di
+
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val dataModule = module {
+
+}
+
+val viewModelModule = module {
+
+}
