92c5ec1f6975650e113e24d464f02f6cd180121a
givemesomecoffee <givemesomecoffee@yandex.ru>
2023-09-02 18:31:22 +0300

    GT-43_added_region_local_search

--- app/src/main/java/ru/practicum/android/diploma/features/filter/ui/screen/region/RegionFilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/filter/ui/screen/region/RegionFilterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.core.ui.lce.ContentState
 import ru.practicum.android.diploma.core.ui.lce.asState
@@ -17,6 +18,8 @@ class RegionFilterViewModel(
     private val _state = MutableLiveData<ContentState<RegionFilterUiState>>()
     val state: LiveData<ContentState<RegionFilterUiState>> = _state
 
+    private val regions = MutableStateFlow<List<RegionUi>>(emptyList())
+
     fun sync(countryId: String?, selectedRegion: AreaResult?) {
         viewModelScope.launch {
             _state.value = ContentState(isLoading = true)
@@ -25,6 +28,7 @@ class RegionFilterViewModel(
                     country.areas?.map { RegionUi.mapFrom(it, country, selectedRegion) }
                         ?: emptyList()
                 }
+                regions.value = regionList
                 RegionFilterUiState(regionList)
             }.asState()
         }
@@ -33,13 +37,27 @@ class RegionFilterViewModel(
     fun toggleSelection(state: RegionUi) {
         val isChecked = !state.isChecked
         val prevContent = _state.value?.content
+        regions.value = regions.value.map {
+            it.copy(isChecked = isChecked && it.region.id == state.region.id)
+        }
         _state.value = _state.value?.copy(
             content = prevContent?.copy(
-                regionList = prevContent.regionList.map {
-                    it.copy(isChecked = isChecked && it.region.id == state.region.id)
-                },
+                regionList = regions.value,
                 hasChanges = true,
                 selectedRegion = state.takeIf { isChecked }
+            )
+        )
+    }
+
+    fun filter(text: String) {
+        val prevContent = _state.value?.content
+        _state.value = _state.value?.copy(
+            content = prevContent?.copy(
+                regionList = if (text.isEmpty()) {
+                    regions.value
+                } else {
+                    regions.value.filter { it.region.name.contains(text) }
+                }
             )
         )
     }
