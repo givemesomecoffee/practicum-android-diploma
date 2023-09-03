fb80101d65708beb012e3a0f27269f0a7841d12c
kukidevs <kukidevs@gmail.com>
2023-09-03 20:45:11 +0400

    Implemented proper salary display & no internet case and blocked screen rotation

--- app/src/main/java/ru/practicum/android/diploma/core/data/models/Salary.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/models/Salary.kt
@@ -1,6 +1,10 @@
 package ru.practicum.android.diploma.core.data.models
 
+import android.content.Context
 import kotlinx.serialization.Serializable
+import ru.practicum.android.diploma.R
+import java.text.NumberFormat
+import java.util.Locale
 
 @Serializable
 data class Salary(
@@ -8,4 +12,41 @@ data class Salary(
     val from: Int?,
     val gross: Boolean?,
     val to: Int?
-)
+) {
+    fun pretty(context: Context): String {
+        var prettyString = ""
+        if (from != null) {
+            prettyString += context.getString(R.string.salary_from, prettifyInt(from)) + " "
+        }
+        if (to != null) {
+            prettyString += context.getString(R.string.salary_to, prettifyInt(to)) + " "
+        }
+        if (prettyString != "") {
+            prettyString += getCurrencySymbol()
+        } else {
+            prettyString = context.getString(R.string.no_salary)
+        }
+        return prettyString
+    }
+
+    private fun prettifyInt(int: Int): String {
+        val numberFormat = NumberFormat.getNumberInstance(Locale.US)
+        return numberFormat.format(int).replace(",", " ")
+    }
+
+    fun getCurrencySymbol(): String {
+        return when (currency) {
+            "RUR"->"₽"
+            "RUB"->"₽"
+            "USD"->"$"
+            "EUR"->"€"
+            "KZT"->"₸"
+            "UAH"->"₴"
+            "AZN"->"₼"
+            "UZS"->"сўм"
+            "GEL"->"₾"
+            else -> currency?:""
+        }
+
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/features/search/data/network/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/search/data/network/SearchRepositoryImpl.kt
@@ -1,15 +1,39 @@
 package ru.practicum.android.diploma.features.search.data.network
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import ru.practicum.android.diploma.features.filter.domain.model.Filter
 import ru.practicum.android.diploma.features.search.data.dto.VacanciesState
 import ru.practicum.android.diploma.features.search.domain.api.SearchRepository
 import ru.practicum.android.diploma.features.search.domain.models.APIQuery
+import ru.practicum.android.diploma.features.search.ui.SearchFragment
 
-class SearchRepositoryImpl(private val api: SearchAPI) : SearchRepository {
+class SearchRepositoryImpl(private val api: SearchAPI, private val context: Context) : SearchRepository {
     override fun getVacancies(query: APIQuery): Flow<VacanciesState> = flow{
+        if (!isConnected()){
+            emit(VacanciesState(SearchFragment.CODE_NO_INTERNET, null))
+            return@flow
+        }
         val response = api.getVacancies(query.toMap())
         emit(VacanciesState(response.code(), response.body()?.items))
+    }
+
+    override fun isConnected(): Boolean {
+        val connectivityManager = context.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            }
+        }
+        return false
     }
 }
