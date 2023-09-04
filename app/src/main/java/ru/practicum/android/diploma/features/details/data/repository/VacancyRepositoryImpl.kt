756fa6a706e16c1e337b7cb0d256406414335559
Igor <igor.sheplyakov@yandex.ru>
2023-09-04 16:37:42 +0300

    убрал хардкод текста

--- app/src/main/java/ru/practicum/android/diploma/features/details/data/repository/VacancyRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/features/details/data/repository/VacancyRepositoryImpl.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.features.details.data.dto.VacancyResponse
 import ru.practicum.android.diploma.features.details.data.network.VacancyApi
 import ru.practicum.android.diploma.features.details.domain.repository.VacancyRepository
@@ -15,7 +16,7 @@ class VacancyRepositoryImpl(
     override suspend fun getVacancy(id: String): Flow<VacancyResponse> = flow {
         try {
             val response = vacancyApi.getVacancy(id)
-            emit(VacancyResponse( response.code(), response.body()))
+            emit(VacancyResponse(response.code(), response.body()))
         } catch (e: Throwable) {
             emit(VacancyResponse(code = -1, vacancy = null))
         }
@@ -28,7 +29,10 @@ class VacancyRepositoryImpl(
             type = "text/plain"
         }
 
-        val shareIntent = Intent.createChooser(sendIntent, "Выберите приложение для отправки")
+        val shareIntent = Intent.createChooser(
+            sendIntent,
+            context.resources.getString(R.string.select_application_to_send)
+        )
         context.startActivity(shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
     }
 }
